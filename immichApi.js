f5e15d466bf13619a3777834fb750a5d9c79b11a
Korey Sedaghatian <4706332+pelaxa@users.noreply.github.com>
2026-07-12 13:42:08 -0400

    fixed #56

--- immichApi.js
+++ immichApi.js
@@ -216,26 +216,37 @@ const immichApi = {
     getAlbumAssets: async function (albumId) {
         let imageList = [];
         try {
-            const response = await this.http.get(this.apiUrls[this.apiLevel]['albumInfo'].replace('{id}',albumId), {responseType: 'json'});
-            if (response.status === 200) {
-                Log.error(LOG_PREFIX + 'Album fetch response ', response.data);
-                // If we are on image v3 and later, then we need to fetch the assets separately
-                if (!!response.data.assets) {
-                    imageList = [...response.data.assets];
-                } else if (this.apiUrls[this.apiLevel].version >= 3) {
-                    // If we are on image v3 and later, then we need to fetch the assets separately
-
-                } else {
-                    Log.error(LOG_PREFIX + 'Oops!  Albums assets are not available due to unexpected API response.  An updated version of this module may be needed to address the issue.');
-                }
-                if (response.data.albumName) {
-                    Log.debug(LOG_PREFIX + `Retrieved ${imageList.length} images for album ${response.data.albumName}`);
-                    imageList.forEach(image =>
-                        image.albumName = response.data.albumName
-                    );
-                }
+            if (this.apiUrls[this.apiLevel].version >= 3) {
+                // For immich v3+, the easiest way to fetch album assets is to use the search function
+                // Why the devs decided to make this harder is beyond me, but the API now is more geared towards the UI rather than being a useful API
+                imageList = await this.searchAssets({
+                                                        type: "IMAGE",
+                                                        query: "*",
+                                                        visibility: "timeline",
+                                                        albumIds: [albumId]
+                                                    });
             } else {
-                Log.error(LOG_PREFIX + 'unexpected response from Immich', response.status, response.statusText);
+                const response = await this.http.get(this.apiUrls[this.apiLevel]['albumInfo'].replace('{id}',albumId), {responseType: 'json'});
+                if (response.status === 200) {
+                    Log.error(LOG_PREFIX + 'Album fetch response ', response.data);
+                    // If we are on image v3 and later, then we need to fetch the assets separately
+                    if (!!response.data.assets) {
+                        imageList = [...response.data.assets];
+                    } else if (this.apiUrls[this.apiLevel].version >= 3) {
+                        // If we are on image v3 and later, then we need to fetch the assets separately
+
+                    } else {
+                        Log.error(LOG_PREFIX + 'Oops!  Albums assets are not available due to unexpected API response.  An updated version of this module may be needed to address the issue.');
+                    }
+                    if (response.data.albumName) {
+                        Log.debug(LOG_PREFIX + `Retrieved ${imageList.length} images for album ${response.data.albumName}`);
+                        imageList.forEach(image =>
+                            image.albumName = response.data.albumName
+                        );
+                    }
+                } else {
+                    Log.error(LOG_PREFIX + 'unexpected response from Immich', response.status, response.statusText);
+                }
             }
         } catch (e) {
             Log.error(LOG_PREFIX + 'Oops!  Exception while fetching pictures from album ', e.message);
@@ -246,21 +257,11 @@ const immichApi = {
 
     getAlbumAssetsForAlbumIds: async function (albumIds) {
         let imageList = [];
-        if (this.apiUrls[this.apiLevel].version >= 3) {
-            // For immich v3+, the easiest way to fetch album assets is to use the search function
-            // Why the devs decided to make this harder is beyond me, but the API now is more geared towards the UI rather than being a useful API
-            imageList = await this.searchAssets({
-                                                    type: "IMAGE",
-                                                    query: "*",
-                                                    visibility: "timeline",
-                                                    albumIds: albumIds
-                                                });
-        } else {
-            for (const albumId of albumIds) {
-                let currentAlbumImages = await this.getAlbumAssets(albumId);
-                if (currentAlbumImages && currentAlbumImages.length > 0) {
-                    imageList = imageList.concat(currentAlbumImages);
-                }
+
+        for (const albumId of albumIds) {
+            let currentAlbumImages = await this.getAlbumAssets(albumId);
+            if (currentAlbumImages && currentAlbumImages.length > 0) {
+                imageList = imageList.concat(currentAlbumImages);
             }
         }
         
@@ -317,7 +318,7 @@ const immichApi = {
     searchAssets: async function (query, size = 1000) {
         let imageList = [];
             
-        Log.debug(LOG_PREFIX + 'Searching for images: ', query, 'SIZE: ', size);
+        // Log.debug(LOG_PREFIX + 'Searching for images: ', query, 'SIZE: ', size);
         try{
             const searchQuery = {...query, size: size};
             Log.debug(LOG_PREFIX + 'Searching query: ', searchQuery);
